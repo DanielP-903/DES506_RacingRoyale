674b628f05cac323bece443b69968d57e069b2db
NICHOLAS SALTER <2101721@uad.ac.uk>
2022-05-31 16:37:30 +0100

    Multiplayer Fix

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,16 +7,22 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView _photonView;
+    private Dan_CarController _dcc;
     // Start is called before the first frame update
     void Start()
     {
         _photonView = GetComponent<PhotonView>();
+        _dcc = GetComponent<Dan_CarController>();
         if (_photonView.IsMine)
         {
             CinemachineVirtualCamera cvc = Camera.main.gameObject.GetComponent<CinemachineVirtualCamera>();
             var transform1 = transform;
             cvc.m_Follow = transform1;
             cvc.m_LookAt = transform1;
+        }
+        else
+        {
+            Destroy(_dcc);
         }
     }
 
