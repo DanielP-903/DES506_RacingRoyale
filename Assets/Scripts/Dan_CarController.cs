7f8c30fa0ea17741e39194ef8b7576085b140ac2
Dan <danpaul309@gmail.com>
2022-05-31 09:18:11 +0100

    Pushed input system package

--- Assets/Scripts/Dan_CarController.cs
+++ Assets/Scripts/Dan_CarController.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Dan_CarController : MonoBehaviour
 {
+    public float maxTorque = 0;
+    public float maxSteeringAngle = 0;
+    public WheelCollider leftWheel;
+    public WheelCollider RightWheel;
+    public bool motor;
+    public bool steering;
+
+
     // Start is called before the first frame update
     void Start()
     {
