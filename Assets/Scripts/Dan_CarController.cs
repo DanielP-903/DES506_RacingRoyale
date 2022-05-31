7406f9e2b45d8db98d6f27787cb8c745ef9ed805
Dan <danpaul309@gmail.com>
2022-05-31 14:38:35 +0100

    Player Car Pushed!
    He do be braking

--- Assets/Scripts/Dan_CarController.cs
+++ Assets/Scripts/Dan_CarController.cs
@@ -8,38 +8,75 @@ using UnityEngine.InputSystem;
 
 public class Dan_CarController : MonoBehaviour
 {
-    public float maxTorque = 0;
+    public float motorForce = 0;
+    public float brakeTorque = 1000;
     public float maxSteeringAngle = 0;
     public List<Axle> axles;
     private bool _moveForward = false;
     private bool _moveBackward = false;
     private bool _moveRight = false;
     private bool _moveLeft = false;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        foreach (var axle in axles)
+        {
+            axle.leftWheel.brakeTorque = brakeTorque;
+            axle.rightWheel.brakeTorque = brakeTorque;
+        }
+    }
 
     public void FixedUpdate()
     {
         float motorMultiplier = _moveForward ? 1 : _moveBackward ? -1 : 0;
-        float currentMotorValue = maxTorque * motorMultiplier;
+        float currentMotorValue = motorForce * motorMultiplier;
         
         float steeringMultiplier = _moveLeft ? -1 : _moveRight ? 1 : 0;
         float currentSteeringValue = maxSteeringAngle * steeringMultiplier;
 
         foreach (var axle in axles)
         {
+            axle.leftWheel.brakeTorque = brakeTorque;
+            axle.rightWheel.brakeTorque = brakeTorque;
             if (axle.steering)
             {
                 axle.leftWheel.steerAngle = currentSteeringValue;
                 axle.rightWheel.steerAngle = currentSteeringValue;
             }
-
             if (axle.motor)
             {
                 axle.leftWheel.motorTorque = currentMotorValue;
                 axle.rightWheel.motorTorque = currentMotorValue;
             }
+
+        }
+
+        if (!_moveForward && !_moveBackward)
+        {
+            brakeTorque = 1000.0f;
+        }
+        else
+        {
+            brakeTorque = 0.0f;
         }
     }
-    
+
+    private void Update()
+    {
+        WheelCollider currentWheel;
+        currentWheel = axles[0].leftWheel;
+        
+        float currentTorque = currentWheel.motorTorque;
+        float currentSteer = currentWheel.steerAngle;
+        float currentBrake = currentWheel.brakeTorque;
+        float currentRpm = currentWheel.rpm;
+        Debug.DrawRay(currentWheel.transform.position, currentWheel.transform.parent.forward * currentTorque / 100, Color.blue);
+        Debug.DrawRay(currentWheel.transform.position, Vector3.up * currentRpm / 100, Color.green);
+        Debug.DrawRay(currentWheel.transform.position, -currentWheel.transform.parent.forward * currentBrake / 100, Color.red);
+    }
+
     // Input Actions
     // W
     public void Forward(InputAction.CallbackContext context)
