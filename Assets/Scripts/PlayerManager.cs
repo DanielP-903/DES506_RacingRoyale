04c7ae0d8ac41c0e149ccae307e1dc12a7b1d6ad
NICHOLAS SALTER <2101721@uad.ac.uk>
2022-06-01 16:56:24 +0100

    Minor Multiplayer Changes

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using Cinemachine;
 using Photon.Pun;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView _photonView;
     private CarController _dcc;
+    
+    [SerializeField]
+    private TextMeshProUGUI playerNameText;
+    [SerializeField]
+    private TextMeshProUGUI playerLicenseText;
+    
+    private PlayerManager target;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +32,21 @@ public class PlayerManager : MonoBehaviour
         else
         {
             Destroy(_dcc);
+        }
+    }
+    
+    public void SetTarget(PlayerManager _target)
+    {
+        if (_target == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+            return;
+        }
+        // Cache references for efficiency
+        target = _target;
+        if (playerNameText != null)
+        {
+            playerNameText.text = target._photonView.Owner.NickName;
         }
     }
 
