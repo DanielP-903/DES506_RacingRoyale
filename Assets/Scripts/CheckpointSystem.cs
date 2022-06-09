fd1dbd765d5bfcb31fc06fa97d89fbc996e24c7b
Dan <danpaul309@gmail.com>
2022-06-09 14:56:20 +0100

    Added checkpoint system

--- Assets/Scripts/CheckpointSystem.cs
+++ Assets/Scripts/CheckpointSystem.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class CheckpointSystem : MonoBehaviour
 {
+    [Header("Note: This defines the order of the checkpoints as well!")]
     public List<GameObject> checkpointObjects = new List<GameObject>();
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
