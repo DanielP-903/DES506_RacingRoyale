284c0c193e0600eb976c60eb26a9e9fc15de4c17
Dan <danpaul309@gmail.com>
2022-06-10 13:28:09 +0100

    Fixed track generator

--- Assets/Scripts/TrackGenerator.cs
+++ Assets/Scripts/TrackGenerator.cs
@@ -25,41 +25,41 @@ public class TrackGenerator : MonoBehaviour
     {
         _generatedTrack.Clear();
         
-        // for (int i = 0; i < noOfPieces; i++)
-        // {
-        //     int rand = Random.Range(0, trackPieces.Count);
-        //     _generatedTrack.Add(Instantiate(trackPieces[rand]));
-        //     
-        // }
-         _generatedTrack.Add(Instantiate(trackPieces[1]));
-         _generatedTrack.Add(Instantiate(trackPieces[3]));
-         _generatedTrack.Add(Instantiate(trackPieces[3]));
-         _generatedTrack.Add(Instantiate(trackPieces[2]));
-         _generatedTrack.Add(Instantiate(trackPieces[3]));
+        for (int i = 0; i < noOfPieces; i++)
+        {
+            int rand = Random.Range(0, trackPieces.Count);
+            _generatedTrack.Add(Instantiate(trackPieces[rand]));
+            
+        }
+         // _generatedTrack.Add(Instantiate(trackPieces[1]));
+         // _generatedTrack.Add(Instantiate(trackPieces[3]));
+         // _generatedTrack.Add(Instantiate(trackPieces[3]));
+         // _generatedTrack.Add(Instantiate(trackPieces[2]));
+         // _generatedTrack.Add(Instantiate(trackPieces[3]));
 
         GenerateTrack();
     }
 
     private void OnDrawGizmos()
     {
-
-        for (int i = 0; i < noOfPieces - 1; i++)
+        if (noOfPieces <= _generatedTrack.Count)
         {
-            // Vector3 forwardPrev = _generatedTrack[i].endLocation.transform.forward;
-            // Vector3 forwardCurrent = _generatedTrack[i + 1].startLocation.transform.forward;
-            
-            directionEnd = transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i].endLocation.transform.forward);
-            directionStart =  transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i+1].startLocation.transform.forward);
-            posEnd = _generatedTrack[i].endLocation.transform.position;
-            posStart = _generatedTrack[i + 1].startLocation.transform.position;
-            
-            Gizmos.color = Color.green;
-            Gizmos.DrawRay(posStart + Vector3.up, directionStart);
-        
-            Gizmos.color = Color.red;
-            Gizmos.DrawRay(posEnd + Vector3.up, directionEnd);
+            for (int i = 0; i < noOfPieces - 1; i++)
+            {
+                directionEnd =
+                    transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i].endLocation.transform.forward);
+                directionStart =
+                    transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i + 1].startLocation.transform.forward);
+                posEnd = _generatedTrack[i].endLocation.transform.position;
+                posStart = _generatedTrack[i + 1].startLocation.transform.position;
+
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(posStart + Vector3.up, directionStart);
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(posEnd + Vector3.up, directionEnd);
+            }
         }
-        
     }
 
     private void GenerateTrack()
@@ -79,23 +79,12 @@ public class TrackGenerator : MonoBehaviour
 
             _generatedTrack[i + 1].transform.position = offset;
             
-            // Get Vector3.Angle between two LOCAL forward vectors
-            //Vector3 localForward = transform.worldToLocalMatrix.MultiplyVector(transform.forward);
-
-            //Vector3 forwardPrev = _generatedTrack[i].endLocation.transform.forward;
-            //Vector3 forwardCurrent = _generatedTrack[i+1].startLocation.transform.forward;            
-            
-            Vector3 forwardPrev = transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i].endLocation.transform.forward);
+            // Get Vector3.SIGNEDAngle between two LOCAL forward vectors at previous end and current start
+            Vector3 forwardPrevEnd = transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i].endLocation.transform.forward);
             Vector3 forwardCurrentStart = transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i+1].startLocation.transform.forward);
-            Vector3 forwardCurrentEnd = transform.worldToLocalMatrix.MultiplyVector(_generatedTrack[i+1].endLocation.transform.forward);
-            float angle = Vector3.Angle(forwardCurrentStart, forwardPrev);
+            
+            float angle = Vector3.SignedAngle(forwardCurrentStart, forwardPrevEnd, Vector3.up);
 
-            if (forwardCurrentStart != forwardCurrentEnd)
-            {
-                angle -= 90;
-            }
-            
-            
             _generatedTrack[i + 1].transform.RotateAround(_generatedTrack[i+1].startLocation.transform.position, Vector3.up,  angle);
         }
     }
