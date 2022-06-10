43f3ac37c2f802fd5f4d1bbab9f90970ad38220d
ferrel kennedy <ferrelkenkan@gmail.com>
2022-06-10 02:25:06 +0100

    testing some obstacles

--- Assets/Scripts/MovingPillars.cs
+++ Assets/Scripts/MovingPillars.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MovingPillars : MonoBehaviour
 {
+    public float speed = 2.5f, distance = 10f;
+    public bool reverse;
+    private Vector3 currentPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(reverse ? -Mathf.PingPong(Time.time * speed, distance) + currentPosition.x : Mathf.PingPong(Time.time * speed, distance) + currentPosition.x, transform.position.y, transform.position.z);
     }
 }
