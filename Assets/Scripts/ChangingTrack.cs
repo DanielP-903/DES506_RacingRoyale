a7a9560a7aa4c12844276ab01d40d09aea7d8f27
creedlegacy <53327659+creedlegacy@users.noreply.github.com>
2022-07-19 11:29:20 +0100

    fixed tracks and stage 1 track adjusts

--- Assets/Scripts/ChangingTrack.cs
+++ Assets/Scripts/ChangingTrack.cs
@@ -4,35 +4,30 @@ using UnityEngine;
 
 public class ChangingTrack : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Version1, Version2, Version3;
     // Start is called before the first frame update
     void Start()
     {
-        if (Version1 != null && Version2 != null && Version3 != null)
+        int version = Random.Range(0, 3);
+        int count = 0;
+        foreach (Transform child in this.transform)
         {
-            
-            Version1.SetActive(false);
-            Version2.SetActive(false);
-            Version2.SetActive(false);
-            int version = Random.Range(0, 3);
-
-            switch (version)
+            if(child.transform != null)
             {
-                case 0:
-                    Version1.SetActive(true);
-                    break;
-                case 1:
-                    Version2.SetActive(true);
-                    break;
-                case 2:
-                    Version3.SetActive(true);
-                    break;
-                default:
-                    Version1.SetActive(true);
-                    break;
+                GameObject currOjbect = child.transform.gameObject;
+                if (count == version)
+                {
+                    currOjbect.SetActive(true);
+                }
+                else
+                {
+                    currOjbect.SetActive(false);
+                }
+                
+                count++;
+
             }
         }
+
 
     }
 
