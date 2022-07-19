cbd471017660047d06fce23c13982ca7094e99a1
NICHOLAS SALTER <2101721@uad.ac.uk>
2022-07-19 11:30:17 +0100

    Added Message Box code

--- Assets/Scripts/MessageBox.cs
+++ Assets/Scripts/MessageBox.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
 public class MessageBox : MonoBehaviour
 {
+    private Queue<string> messages;
+    private TextMeshProUGUI[] messageBoxes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        messageBoxes = transform.GetChild(0).GetChild(0).GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (TextMeshProUGUI tmp in messageBoxes)
+        {
+            tmp.text = "";
+        }
     }
 
     // Update is called once per frame
